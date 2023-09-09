e178fb8bc9c52f038b7ad2adcae7a26a802b3b65
Vidit Khandelwal <viditk17@gmail.com>
2023-09-09 02:00:38 -0400

    minor update

--- src/components/Music.tsx
+++ src/components/Music.tsx
@@ -43,7 +43,7 @@ const Music = () => {
           <div className="text-slate-400">Taylor Swift</div>
         </div>
         <Link
-          className="group mr-8 flex flex-row items-center p-2 rounded-full border border-slate-300 transition-all duration-200 ease-in-out hover:ring hover:ring-slate-300 hover:border-slate-600"
+          className="group flex flex-row items-center p-2 rounded-full border border-slate-300 transition-all duration-200 ease-in-out hover:ring hover:ring-slate-300 hover:border-slate-600"
           href="https://open.spotify.com/track/1fo2ctLqj3zBhRQKOXprol?si=616958a98ed2439e"
           rel="noreferrer noopener"
           target="_blank"
